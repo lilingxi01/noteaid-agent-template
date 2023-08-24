082a8a6e85f10c5b11070b949ee7b6bee6bbad8f
Lingxi Li <lilingxi01@gmail.com>
2023-08-24 09:36:32 -0400

    feat: add type safety.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
 import json
+import logging
 
+from pydantic import ValidationError
 from handler import MyAgent
+from notebridge import ChatMessage, ChatContext
 
 
 def handler(event, context):
@@ -8,4 +11,39 @@ def handler(event, context):
 
     print("Received event:", json.dumps(event, indent=2))
 
-    return {"statusCode": 200, "body": "hello world"}
+    if 'message_stack' not in event or type(event['message_stack']) != list:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "message": "Message stack (message_stack) is invalid from the request."
+            })
+        }
+
+    if 'context' not in event or type(event['context']) != dict:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "message": "Context (context) is invalid from the request."
+            })
+        }
+
+    try:
+        message_stack = [ChatMessage(**m) for m in event['message_stack']]
+        context = ChatContext(**event['context'])
+    except ValidationError as e:
+        logging.error(f"Error occurred while parsing the request: {str(e)}")
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "message": f"Invalid message_stack or context structure: {str(e)}"
+            })
+        }
+
+    response = agent.on_receive(message_stack=message_stack, context=context)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "response": response
+        })
+    }
--- handler.py
+++ handler.py
@@ -6,4 +6,4 @@ from notebridge import Bridge, ChatMessage, ChatContext
 class MyAgent(Bridge):
     def on_receive(self, message_stack: List[ChatMessage], context: ChatContext) -> Union[str, List[str]]:
         # TODO: Implement your agent here.
-        pass
+        return 'Hello, world!'
