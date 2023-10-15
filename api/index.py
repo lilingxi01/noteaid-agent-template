027da0ee282dd1cabb04ab54adc79f8bf12c44cb
Lingxi Li <lilingxi01@gmail.com>
2023-10-15 15:17:23 -0400

    feat: add OPENAI_ORG_ID support and env check.

--- api/index.py
+++ api/index.py
@@ -5,6 +5,13 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from dotenv import load_dotenv
 load_dotenv(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '.env'))
 
+if os.getenv("OPENAI_ORG_ID") is None or os.getenv("OPENAI_API_KEY") is None:
+    raise Exception("OPENAI_ORG_ID and OPENAI_API_KEY must be set in the .env file before use.")
+
+import openai
+openai.organization = os.getenv("OPENAI_ORG_ID")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 from flask import Flask
 from flask_cors import CORS
 from handler import MyAgent
